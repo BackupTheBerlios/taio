759beb69a710873189d4f23bb19fe7699ad3e43d
maciejk <maciejk>
2007-05-02 22:01:33 +0000

    *** empty log message ***

---
+++ taio/taio/tests/test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taio.tests
+{
+    class test
+    {
+        Data.Solution s;
+
+        public test(Data.Solution s)
+        {
+            this.s = s;
+        }
+    }
+}
